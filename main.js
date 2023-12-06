e8605995da71ce679da655f4460f16e0c00d6cdd
alex512-cr <75994424+alex512-cr@users.noreply.github.com>
2023-12-06 14:49:49 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const radio1_btn = document.getElementById("id1");
 const radio2_btn = document.getElementById("id2");
 const radio3_btn = document.getElementById("id3");
 
+const submit_btn = document.getElementById("submit");
+
+let tg = window.Telegram.WebApp;
+console.log(tg.initDataUnsafe.user.username);
+
 radio1_btn.onclick = () => {
 	radio1_btn.classList.add("radio_focus");
 	radio2_btn.classList.remove("radio_focus");
@@ -19,3 +24,7 @@ radio3_btn.onclick = () => {
 	radio2_btn.classList.remove("radio_focus");
 	radio3_btn.classList.add("radio_focus");
 }
+
+submit_btn.onclick = () => {
+	console.log("yes");
+} 
