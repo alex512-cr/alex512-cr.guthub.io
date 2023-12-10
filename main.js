a5728cf48dbf6f9ee680becd50ff71e4116d55c4
alex512-cr <75994424+alex512-cr@users.noreply.github.com>
2023-12-10 21:53:58 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,16 +3,21 @@ const radio2_btn = document.getElementById("id2");
 const radio3_btn = document.getElementById("id3");
 
 const address_input = document.getElementById("address_input");
+const number_input = document.getElementById("input_number_value");
 
 const submit_btn = document.getElementById("submit");
 
 let tg = window.Telegram.WebApp;
 tg.ready();
+
+let radio = 1;
+
 radio1_btn.onclick = () => {
 	radio1_btn.classList.add("radio_focus");
 	radio2_btn.classList.remove("radio_focus");
 	radio3_btn.classList.remove("radio_focus");
 	address_input.classList.add("address_form_disabled");
+	radio = 1;
 }
 
 radio2_btn.onclick = () => {
@@ -20,6 +25,7 @@ radio2_btn.onclick = () => {
 	radio2_btn.classList.add("radio_focus");
 	radio3_btn.classList.remove("radio_focus");
 	address_input.classList.remove("address_form_disabled");
+	radio = 2;
 }
 
 radio3_btn.onclick = () => {
@@ -27,10 +33,24 @@ radio3_btn.onclick = () => {
 	radio2_btn.classList.remove("radio_focus");
 	radio3_btn.classList.add("radio_focus");
 	address_input.classList.remove("address_form_disabled");
+	radio = 3;
 }
 
 submit_btn.onclick = () => {
-	console.log("yes");
+	let delivery_address = '';
+	let number_of_calendars = 0;
+	let delivery_address_input = document.getElementById("input_address_value");
+	if (radio != 1) {
+		delivery_address = delivery_address_input.value;
+	}
+	number_of_calendars = number_input.value;
+	console.log(delivery_address, number_of_calendars)
+	let data = {
+		username: tg.initDataUnsafe.user.username,
+		method_of_obtaining: radio,
+		delivery_address: delivery_address,
+		number_of_calendars: number_of_calendars
+	}
+	tg.sendData(JSON.stringify(data));
+	tg.close();
 }
-
-console.log(tg.initDataUnsafe.user);
