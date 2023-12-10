25f5d01693c867d041a76f35f92b2834063dae27
alex512-cr <75994424+alex512-cr@users.noreply.github.com>
2023-12-10 20:42:53 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,31 +2,36 @@ const radio1_btn = document.getElementById("id1");
 const radio2_btn = document.getElementById("id2");
 const radio3_btn = document.getElementById("id3");
 
+const address_input = document.getElementById("address_input");
+
 const submit_btn = document.getElementById("submit");
 
 let tg = window.Telegram.WebApp;
 console.log(tg);
 //console.log(tg);
-/*radio1_btn.onclick = () => {
+radio1_btn.onclick = () => {
 	radio1_btn.classList.add("radio_focus");
 	radio2_btn.classList.remove("radio_focus");
 	radio3_btn.classList.remove("radio_focus");
+	address_input.classList.add("address_form_disabled");
 }
 
 radio2_btn.onclick = () => {
 	radio1_btn.classList.remove("radio_focus");
 	radio2_btn.classList.add("radio_focus");
 	radio3_btn.classList.remove("radio_focus");
+	address_input.classList.remove("address_form_disabled");
 }
 
 radio3_btn.onclick = () => {
 	radio1_btn.classList.remove("radio_focus");
 	radio2_btn.classList.remove("radio_focus");
 	radio3_btn.classList.add("radio_focus");
+	address_input.classList.remove("address_form_disabled");
 }
 
 submit_btn.onclick = () => {
 	console.log("yes");
-}*/
+}
 
-tg.showAlert(WebApp.WebAppUser.username);
+console.log(WebApp.WebAppUser.username);
